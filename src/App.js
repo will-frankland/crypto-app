5db257e4469304023d747004a658ad6d8e08b0b1
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-04 02:37:37 -0500

    change switch to routes to comply with react-router-dom v6. Update code to reflect inside App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,38 @@
-import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
-import { Layout, Typography, Space } from 'antd';
-import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components';
-import './App.css'
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  News,
+  CryptoDetails,
+} from "./components";
+import "./App.css";
 
 const App = () => {
   return (
-    <div className='app'>
-      <div className='navbar'>
+    <div className="app">
+      <div className="navbar">
         <Navbar />
       </div>
-      <div className='main'>
+      <div className="main">
         <Layout>
-          <div className='routes'>
-            <Switch>
-              <Route exact path='/'>
-                <Homepage />
-              </Route>
-              <Route exact path='/exchanges'>
-                <Exchanges />
-              </Route>
-              <Route exact path='/cryptocurrencies'>
-                <Cryptocurrencies />
-              </Route>
-              <Route exact path='/crypto/:coinId'>
-                <CryptoDetails />
-              </Route>
-              <Route exact path='/news'>
-                <News />
-              </Route>
-            </Switch>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route exact path="/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
           </div>
         </Layout>
       </div>
-      <div className='footer'>
-
-      </div>
-      
+      <div className="footer"></div>
     </div>
-  )
+  );
 };
 
 export default App;
