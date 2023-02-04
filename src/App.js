bba1f7e157476ebfa0a54a3d306faecdb23e184d
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-04 02:26:09 -0500

    create each component required to match previously components in app.jsx, export all from index.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
-import { Navbar } from './components';
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components';
 import './App.css'
 
 const App = () => {
