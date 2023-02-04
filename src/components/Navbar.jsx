6b2dd23e356b7b4b74ea5d7b679e7374fecbdc5a
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-04 02:16:43 -0500

    Create menu in Navbar with link to Home, Cryptocurrencies, Exchanges and News

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,10 +11,21 @@ const Navbar = () => {
         <Avatar src={icon} size='large'/>
         <Typography.Title level={2}  className='logo'/>
         <Link to='/'>Cryptoverse</Link>
-        {/* <Button className='menu-control-container'>
-
-        </Button> */}
       </div>
+      <Menu theme='dark'>
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to='/'>Home</Link>
+        </Menu.Item>
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
+        </Menu.Item>
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link to='/exchanges'>Exchanges</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to='/news'>News</Link>
+        </Menu.Item>
+      </Menu>
     </div>
   )
 }
