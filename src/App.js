8d21ccc0e9abcd8a28997925e7ad6b98b256883f
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-04 02:20:51 -0500

    add components to App.js with path: Homepage, Exchanges, Cryptocurrencies, CryptoDetails and News

--- src/App.js
+++ src/App.js
@@ -11,7 +11,27 @@ const App = () => {
         <Navbar />
       </div>
       <div className='main'>
-
+        <Layout>
+          <div className='routes'>
+            <Switch>
+              <Route exact path='/'>
+                <Homepage />
+              </Route>
+              <Route exact path='/exchanges'>
+                <Exchanges />
+              </Route>
+              <Route exact path='/cryptocurrencies'>
+                <Cryptocurrencies />
+              </Route>
+              <Route exact path='/crypto/:coinId'>
+                <CryptoDetails />
+              </Route>
+              <Route exact path='/news'>
+                <News />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
       </div>
       <div className='footer'>
 
