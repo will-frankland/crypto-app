6312a378859b885689a2cfb0cc9308906f49297a
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-04 11:03:12 -0500

    create cryptoApiHeaders function and add headers taken from coinranking code, create baseUrl constant and export cryptoAPI

--- src/services/CryptoApi.js
+++ src/services/CryptoApi.js
@@ -1,17 +1,12 @@
-// const options = {
-//   method: 'GET',
-//   url: 'https://coinranking1.p.rapidapi.com/coins',
-//   params: {
-//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
-//     timePeriod: '24h',
-//     'tiers[0]': '1',
-//     orderBy: 'marketCap',
-//     orderDirection: 'desc',
-//     limit: '50',
-//     offset: '0'
-//   },
-//   headers: {
-//     'X-RapidAPI-Key': '3f27f651bdmsh22b944e7767ab77p13eb38jsn7a99c6ec1d66',
-//     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-//   }
-// };
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+
+const cryptoApiHeaders = {
+  'X-RapidAPI-Key': '3f27f651bdmsh22b944e7767ab77p13eb38jsn7a99c6ec1d66',
+  'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/coins'
+
+export const cryptoApi = createApi({
+  
+});
