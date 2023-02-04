529275e927cea9feb5a27497e613485df1e45e24
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-04 15:33:37 -0500

    add new div for Top 10 global cryptos along with show more link directing user to /cryptocurrencies

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -23,6 +23,10 @@ const Homepage = () => {
       <Col span={12}><Statistic title='Total 24 Hour Volume' value={millify(globalStats.total24hVolume)}/></Col>
       <Col span={12}><Statistic title='Total Markets' value={millify(globalStats.totalMarkets)}/></Col>
     </Row>
+    <div className='home-heading-container'>
+      <Title level={2} className='home-title'>Top 10 Global Cryptocurrencies</Title>
+      <Title level={3} className='show-more'><Link to='/cryptocurrencies'>Show More</Link></Title>
+    </div>
     </>
   )
 }
