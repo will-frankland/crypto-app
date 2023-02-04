a5bd0c9e4cedac69344a7103d6782a48aff7d41d
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-04 01:51:04 -0500

    create index.js inside components to allow efficient import of components to App.js. Also import Navbar and use in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Switch, Router, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
+import { Navbar, Layout, Footer } from './components';
 
 const App = () => {
   return (
     <div className='app'>
       <div className='navbar'>
-
+        <Navbar />
       </div>
       <div className='main'>
 
