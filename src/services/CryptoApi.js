8d86feaf1d80f7ff0bac25f0caf05f2532612c44
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-04 10:55:36 -0500

    add services folder with coinranking options function into CryptoApi dir

---
+++ src/services/CryptoApi.js
@@ -0,0 +1,17 @@
+// const options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/coins',
+//   params: {
+//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//     timePeriod: '24h',
+//     'tiers[0]': '1',
+//     orderBy: 'marketCap',
+//     orderDirection: 'desc',
+//     limit: '50',
+//     offset: '0'
+//   },
+//   headers: {
+//     'X-RapidAPI-Key': '3f27f651bdmsh22b944e7767ab77p13eb38jsn7a99c6ec1d66',
+//     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//   }
+// };
