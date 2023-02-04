fca994c36c834ff6c3625ad3d887e2881dd11ee4
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-04 15:36:37 -0500

    duplicate div and now show news with link to News page

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,6 +4,7 @@ import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import { Cryptocurrencies, News } from '../components'
 
 const { Title } = Typography;
 
@@ -27,6 +28,12 @@ const Homepage = () => {
       <Title level={2} className='home-title'>Top 10 Global Cryptocurrencies</Title>
       <Title level={3} className='show-more'><Link to='/cryptocurrencies'>Show More</Link></Title>
     </div>
+    <Cryptocurrencies simplified/>
+    <div className='home-heading-container'>
+      <Title level={2} className='home-title'>Latest Crypto News</Title>
+      <Title level={3} className='show-more'><Link to='/news'>Show More</Link></Title>
+    </div>
+    <News simplified/>
     </>
   )
 }
