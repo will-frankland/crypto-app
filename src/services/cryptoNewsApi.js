bdd70033fbb8f9d51dd054b5ff8ebcb264c5573a
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-06 14:59:06 -0500

    update headers and query string in cryptoNewsApi.js to conform to new api standards

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,11 +1,14 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoNewsHeaders = {
+  'x-rapidapi-subscription': 'ultra',
+  'x-rapidapi-proxy-secret': 'c02cea90-4588-11eb-add9-c577b8ecdc8e',
+  'x-rapidapi-user': 'suprikurniyanto',
   'X-RapidAPI-Key': '3f27f651bdmsh22b944e7767ab77p13eb38jsn7a99c6ec1d66',
   'X-RapidAPI-Host': 'news-api14.p.rapidapi.com'
 };
 
-const baseUrl = 'https://news-api14.p.rapidapi.com/top-headlines'
+const baseUrl = 'https://news-api14.p.rapidapi.com/'
 
 const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
 
@@ -14,7 +17,7 @@ export const cryptoNewsApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptoNews: builder.query({
-      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
+      query: ({ newsCategory, count }) => createRequest(`/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
     })
   })
 });
