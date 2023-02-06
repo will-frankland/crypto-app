9fe06f658ff8c3ff9f08054dca8644fea0b5abf2
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-06 14:18:12 -0500

    add styling to news card for homepage

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -10,12 +10,22 @@ const News = ({ simplified }) => {
   const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 10 : 100 });
 
   console.log('cryptoNews->', cryptoNews);
-
   if(!cryptoNews?.value) return 'Loading ...';
+
   return (
-    <div>
-      News
-    </div>
+    <Row gutter={[24, 24]}>
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className='news-card'>
+            <a href={news.url} target="_blank" ref="noreferrer">
+              <div className='news-image-container'>
+                <Title className='news-title' level={4}>{news.name}</Title>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
   )
 }
 
