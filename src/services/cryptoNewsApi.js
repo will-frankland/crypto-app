031366e3b04f8fed11a756f9b3a09cb79653d2f7
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-06 13:39:27 -0500

    create access to News Api using redux in similar fashion to crypto API call

--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,5 +1,25 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
 
 const cryptoNewsHeaders = {
-  
+    'X-BingApis-SDK': 'true',
+    'X-RapidAPI-Key': '3f27f651bdmsh22b944e7767ab77p13eb38jsn7a99c6ec1d66',
+    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
 };
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptNewsApi = createApi({
+  reducerPath: 'cryptoNewsApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`)
+    })
+  })
+});
+
+export const {
+  useGetCryptoNewsQuery
+} = cryptNewsApi;
