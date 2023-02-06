de3a6c4f505774c2eb71baed0e50ed4108b9b276
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-05 23:44:26 -0500

    add if statement to remove search functionality from homepage

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -7,23 +7,30 @@ import { useGetCryptosQuery } from "../services/cryptoApi";
 const Cryptocurrencies = ({ simplified }) => {
   const count = simplified ? 10 : 100;
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
-  const [ cryptos, setCryptos ] = useState();
-  const [ searchTerm, setSearchTerm] = useState('');
+  const [cryptos, setCryptos] = useState();
+  const [searchTerm, setSearchTerm] = useState("");
 
   console.log("cryptos->", cryptos);
 
   useEffect(() => {
-    const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
-    setCryptos(filteredData)
+    const filteredData = cryptosList?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setCryptos(filteredData);
   }, [cryptosList, searchTerm]);
 
-  if(isFetching) return 'Loading ...';
+  if (isFetching) return "Loading ...";
 
   return (
     <>
-    <div className="search-crypto">
-      <Input placeholder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value)} />
-    </div>
+      {!simplified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
