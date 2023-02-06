7127f1f343fb086d9ecddc8f4897bc93829c65a6
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-06 13:42:24 -0500

    import useGetCryptoNews to News.js and create variable to utilize function with parameters

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
 import moment from 'moment';
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+
+const { Text, Title } = Typography;
+const { Option } = Select;
 
 const News = () => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count});
   return (
     <div>
       News
