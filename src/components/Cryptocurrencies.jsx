247ee87d6233e69b6ae202844b9b10d041cb03fd
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-05 23:31:11 -0500

    add code to display top 10 coins on homepage utilzing simplified and conditional operator

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -4,15 +4,16 @@ import { Link } from "react-router-dom";
 import { Card, Row, Col, Input } from "antd";
 import { useGetCryptosQuery } from "../services/cryptoApi";
 
-const Cryptocurrencies = () => {
-  const { data: cryptosList, isFetching } = useGetCryptosQuery();
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
   const [ cryptos, setCryptos ] = useState(cryptosList?.data?.coins);
 
   console.log("cryptos->", cryptos);
 
   return (
     <>
-      <Row gutters={[32, 32]} className="crypto-card-container">
+      <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
             <Link to={`/crypto/${currency.id}`}>
